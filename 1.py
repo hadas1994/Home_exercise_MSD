63e64f8fe25b2adfcf4b05ac0f221a1e24e91610
Hadas <zhadas83@gmail.com>
2022-09-06 13:59:05 +0300

    upload file

--- 1.py
+++ 1.py
@@ -1,2 +1,10 @@
-import datetime
-print(datetime.datetime.now())
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route('/')
+def hello():
+    return 'Hello World!'
+
+
+app.run(host='127.0.0.1', debug=True, port=5000)
